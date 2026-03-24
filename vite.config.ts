364cc43ef6f8932b0be4c956d16fe2e32fb56ebe
Victor Cheng <victorchengthesecond@gmail.com>
2026-03-24 14:41:35 -0700

    Allow hosted style.dev Vite hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [".style.dev"],
+  },
+  preview: {
+    allowedHosts: [".style.dev"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
